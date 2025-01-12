961aae367efb84b11fc2902831f5059d08b1a657
Mahabubur Rahman <microronyy@gmail.com>
2025-01-12 13:02:07 +0600

    remove critical css lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import criticalCss from 'astro-critical-css';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), criticalCss()],
+    integrations: [mdx(), sitemap()],
 });
