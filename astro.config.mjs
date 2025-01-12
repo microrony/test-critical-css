7bb8b74a9c2d2a32726e3ef6e1db47572f421bff
Mahabubur Rahman <microronyy@gmail.com>
2025-01-12 13:03:17 +0600

    add another critical css lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 
+import playformInline from '@playform/inline';
+
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), playformInline()],
 });
